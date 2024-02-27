90138961b3d1f3c55539ca604987f7c74f9633f8
KudeKube <Kuutti.taavitsainen@icloud.com>
2024-02-27 08:32:50 +0200

    Initial Commit

---
+++ Metal2D/Metal2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal2DApp.swift
+//  Metal2D
+//
+//  Created by Kuutti Taavitsainen on 27.2.2024.
+//
+
+import SwiftUI
+
+@main
+struct Metal2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
